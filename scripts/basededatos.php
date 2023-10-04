53ff61251570d60b43b5c7cdb3c6b92b9c4864cd
IbaiS94 <98406330+IbaiS94@users.noreply.github.com>
2023-10-04 21:33:15 +0200

    Add files via upload

---
+++ scripts/basededatos.php
@@ -0,0 +1,25 @@
+<?php
+$host = "localhost";
+$usuario = "juegosacceso";
+$contrasena = "admin";
+$maria = "juegos";
+$dbconnect=mysqli_connect($host,$usuario,$contrasena,$maria);
+if($dbconnect->connect_error){
+	die("error de conexion");
+}
+if(isset($_POST['submit'])) {
+  $nombre=$_POST['nombre'];
+  $desarrollador=$_POST['desa'];
+  $puntuacion=$_POST['puntu'];
+  $genero=$_POST['gen'];
+  $ano=$_POST['anno'];
+  $link=$_POST['link'];
+$q = "INSERT INTO juegosAnadidos (Nombre, Desarrollador, Puntuacion, Genero, Ano, Link)
+  VALUES ('$nombre', '$desarrollador', '$puntuacion','$genero','$ano','$link')";
+     if (!mysqli_query($dbconnect, $q)) {
+        die('An error occurred when submitting your review.');
+    } else {
+      echo "Thanks for your review.";
+    }
+}
+?>
