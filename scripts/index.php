92e09570357d822df3af896efc0fd95317d07ff8
IbaiS94 <98406330+IbaiS94@users.noreply.github.com>
2023-10-04 23:07:30 +0200

    Add files via upload
    PHP LISTO

--- scripts/index.php
+++ scripts/index.php
@@ -7,14 +7,14 @@ $dbconnect=mysqli_connect($host,$usuario,$contrasena,$maria);
 if($dbconnect->connect_error){
 	die("error de conexion");
 }
-if(isset($_POST['submit'])) {
+if(isset($_POST['enviar'])) {
   $nombre=$_POST['nombre'];
   $desarrollador=$_POST['desa'];
   $puntuacion=$_POST['puntu'];
   $genero=$_POST['gen'];
   $ano=$_POST['anno'];
   $link=$_POST['link'];
-$q = "INSERT INTO juegosAnadidos (Nombre, Desarrollador, Puntuacion, Genero, Ano, Link) VALUES ('nombre', 'desarrollador', '1999,'DIOS','0','a.com')";
+$q = "INSERT INTO juegosAnadidos (Nombre, Desarrollador, Puntuacion, Genero, Ano, Link) VALUES ('$nombre', '$desarrollador', '$puntuacion','$genero','$ano','$link')";
       if (mysqli_query($dbconnect, $q)) {
         echo "Registro insertado correctamente.";
     } else {
