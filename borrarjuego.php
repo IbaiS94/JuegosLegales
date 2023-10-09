161af0ce8bf41941eed2e16cf2cb3b1eec3c9d31
cursedx0 <104449990+cursedx0@users.noreply.github.com>
2023-10-09 18:05:59 +0200

    Creatación borrarjuego.php
    Confirmación antes de borrar un juego.

---
+++ borrarjuego.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Juegos Legales</title>
+<link rel="stylesheet" href="styles.css">
+<!--<link rel="icon" type="image/x-icon" href="skullspining.png">-->
+</head>
+
+<body>
+<div class="toptitle">
+<img src="images/skullspining.png" alt="skull" style="float:left;">
+<img src="images/skullspining.png" alt="skull" style="float:right;">
+<h1>Juegos Legales</h1>
+</div>
+
+<!-- <h3>La legalidad es lo primero</h3> -->
+
+<div class="topmenu">
+    <a href="index.html">Home</a>
+    <a href="juegos.php">Juegos</a>
+    <a href="login.html">Log in</a>
+    <a href="signin.html">Sign in</a>
+    <a href="about.html">About</a>
+</div>
+
+<h2>Borrar juego</h2>
+        
+        <div class="cajaborrado">
+        <form action="borrando.php" name="borrar" method="post">
+            <br>
+            <?php
+                /*include 'juegos.php';*/
+                /*$nom = $nombre;*/
+                $nomjuego = $_POST['Nombre'];
+                echo "<br>";
+                echo "¿Seguro que quieres borrar el juego '";
+                echo $_POST["Nombre"];
+                echo "'?<br><br>";
+            ?>
+            <input type='hidden' name='nom' value=" <?php echo $nomjuego; ?> ">
+            <input class="botongeneral" type="submit" name="borrar" value="Borrar">
+        </form>
+        </div>
+</body>
+
+</html>
