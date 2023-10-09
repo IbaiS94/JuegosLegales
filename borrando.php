0c12571925b2e1cd38a75e125954e550cfe69fe9
cursedx0 <104449990+cursedx0@users.noreply.github.com>
2023-10-09 18:08:40 +0200

    Creación de borrando.php
    Página donde se borra el juego tras la confirmación.

---
+++ borrando.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Juegos Legales</title>
+<link rel="stylesheet" href="styles.css">
+<!--<link rel="icon" type="image/x-icon" href="skullspining.png">-->
+</head>
+
+<body>
+<div class="toptitle">
+<img src="images/skullspining.png" alt="skull" style="float:left;">
+<img src="images/skullspining.png" alt="skull" style="float:right;">
+<h1>Juegos Legales</h1>
+</div>
+
+<!-- <h3>La legalidad es lo primero</h3> -->
+
+<div class="topmenu">
+    <a href="index.html">Home</a>
+    <a href="juegos.php">Juegos</a>
+    <a href="login.html">Log in</a>
+    <a href="signin.html">Sign in</a>
+    <a href="about.html">About</a>
+</div>
+
+<?php
+    /*echo $_POST['nom'];*/
+    $nom = $_POST['nom'];
+    //echo $nom;
+    $nom = substr($nom, 1);
+    $username='juegosacceso';
+    $password='admin';
+    $host='localhost';
+    $db = 'juegos';
+    $conn = mysqli_connect($host,$username,$password,$db)  or die("No se pudo conectar con la base de datos.");
+    $query = "DELETE FROM juegosAnadidos WHERE Nombre='".$nom."'";
+    $resultado = mysqli_query($conn, $query);/*/devueve booleano*/
+    
+    
+    
+    if($resultado){
+    	echo "<h2>Juego borrado</h2>
+    	<div class='cajaborrado'>
+    		<br>
+    		<p>Juego eliminado correctamente.</p>
+    		<br>
+    		<a href=juegos.php class='enlacecentral'>Volver a Juegos</a>
+    	</div>";
+    }else{
+    	echo "<h2>Juego no borrado</h2>
+    	<div class='cajaborrado'>
+    		<br>
+    		<p>No se ha podido eliminar el juego.</p>
+    		<br>
+    		<a href=juegos.php class='enlacecentral'>Volver a Juegos</a>
+    	</div>";
+    }
+
+mysqli_close($conn);
+?>
+
+
+        
+</body>
+
+</html>
