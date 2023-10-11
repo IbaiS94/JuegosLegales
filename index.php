0e2cea41bf561606f86b2e38067123cee15fe0a0
cursedx0 <104449990+cursedx0@users.noreply.github.com>
2023-10-11 12:44:19 +0200

    Update index.php
    Cambios que mejoran la estética.

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Juegos Legales</title>
+<link rel="stylesheet" href="styles.css">
+<!--<link rel="icon" type="image/x-icon" href="skullspining.png">-->
+</head>
+
+<body>
+<div class="toptitle">
+<img src="images/skullspining.png" alt="skull" style="float:left;">
+<img src="images/skullspining.png" alt="skull" style="float:right;">
+<h1>Juegos Legales</h1>
+</div>
+
+<!-- <h3>La legalidad es lo primero</h3> -->
+
+<div class="topmenu">
+    <a href="index.html">Home</a>
+    <a href="juegos.php">Juegos</a>
+    <a href="login.html">Log in</a>
+    <a href="signin.html">Sign in</a>
+    <a href="about.html">About</a>
+</div>
+
 <?php
 $host = "localhost";
 $usuario = "juegosacceso";
@@ -7,7 +32,7 @@ $dbconnect=mysqli_connect($host,$usuario,$contrasena,$maria);
 if($dbconnect->connect_error){
 	die("error de conexion");
 }
-if(isset($_POST['enviar'])) {
+if(isset($_POST['confirmar'])) {
   $nombre=$_POST['nombre'];
   $desarrollador=$_POST['desa'];
   $puntuacion=$_POST['puntu'];
@@ -16,9 +41,35 @@ if(isset($_POST['enviar'])) {
   $link=$_POST['link'];
 $q = "INSERT INTO juegosAnadidos (Nombre, Desarrollador, Puntuacion, Genero, Ano, Link) VALUES ('$nombre', '$desarrollador', '$puntuacion','$genero','$ano','$link')";
       if (mysqli_query($dbconnect, $q)) {
-        echo "Registro insertado correctamente.";
+        echo "<h2>Juego a&ntilde;adido</h2>
+    	<div class='cajaborrado'>
+    		<br>
+    		<p>Juego a&ntilde;adido correctamente.</p>
+    		<br>
+    		<a href=juegos.php class='enlacecentral'>Volver a Juegos</a>
+    	</div>";
     } else {
-        echo "Error al insertar el registro: " . mysqli_error($dbconnect);
+    	echo "<h2>Juego no a&ntilde;adido</h2>
+    	<div class='cajaborrado'>
+    		<br>
+    		<p>Error al a&ntilde;adir el juego: " . mysqli_error($dbconnect);"</p>
+    		<br>
+    		<a href=juegos.php class='enlacecentral'>Volver a Juegos</a>
+    	</div>";
+        //echo "Error al a&ntilde;adir el juego: " . mysqli_error($dbconnect);
 }
-}
+}else{
+	echo "<h2>Error</h2>
+    	<div class='cajaborrado'>
+    		<br>
+    		<p>Algo fue mal.</p>
+    		<br>
+    		<a href=juegos.php class='enlacecentral'>Volver a Juegos</a>
+    	</div>";
+} 
+mysqli_close($dbconnect);
 ?>
+
+</body>
+
+</html>
