af51436a24a00fd775f7e1a97e8c2c74199e5c11
cursedx0 <104449990+cursedx0@users.noreply.github.com>
2023-10-11 12:46:06 +0200

    Update comprobardatosjuego.js
    Añadidos limites y controles para el resto de campos.

--- scripts/comprobardatosjuego.js
+++ scripts/comprobardatosjuego.js
@@ -1,26 +1,40 @@
 function comprobardatosjuego(){
-    //var nom = document.formulario.nombre.value;
-    //var desa = document.formulario.desa.value;
+    var nom = document.formulario.nombre.value;
+    var desa = document.formulario.desa.value;
     var puntu = document.formulario.puntu.value;
-    //var gen = document.formulario.gen.value;
+    var gen = document.formulario.gen.value;
     var anno = document.formulario.anno.value;
-    //var link = document.formulario.link.value;
+    var link = document.formulario.link.value;
 
     var mensaje = "Problemas con los datos: \n"
     var error = false;
 
     var y = /\D/g;
+    if(nom.length==0){
+    	error=true;
+    	mensaje = mensaje.concat("Introduzca un nombre. \n")
+    }
     
-    if(y.test(puntu)||(puntu>10)||(puntu<0)){
+    if(desa.length==0){
+    	error=true;
+    	mensaje = mensaje.concat("Introduzca un desarrollador. \n")
+    }
+    
+    if(y.test(puntu)||(puntu>10)||(puntu<0)||(puntu.length==0)){
         error=true
         mensaje = mensaje.concat("Introduzca una puntuación válida. \n")
     }
 
     var currentTime = new Date();
     var year = currentTime.getFullYear()
-    if(y.test(anno)||(anno>year)||(anno<1971)){
+    if(y.test(anno)||(anno>year)||(anno<1971)||(anno.length==0)){
         error=true
         mensaje = mensaje.concat("Introduzca una año válido. \n")
+    }
+    
+    if(link.length==0){
+    	error=true;
+    	mensaje = mensaje.concat("Introduzca un link. \n")
     }
 
 
@@ -28,7 +42,7 @@ function comprobardatosjuego(){
         //window.alert(anno.match(y))
         window.alert(mensaje)
     }else{
-        document.registro.submit();
+        document.formulario.submit();
         //window.alert("mola")//debug
     }
 }
