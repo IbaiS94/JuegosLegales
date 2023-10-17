cbd573ecf228fba20a4372a2d62ae650e7453e8e
IbaiS94 <98406330+IbaiS94@users.noreply.github.com>
2023-10-17 17:55:01 +0200

    arreglao

--- iniciado.php
+++ iniciado.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script type="text/javascript" src="scripts/comprobar2.js"></script>
-<link rel="stylesheet" href="styles.css">
-<link rel="icon" type="image/x-icon" href="images/favicon.ico">
-</head>
-
-<body>
-
-<div class="toptitle">
-<img src="images/skullspining.png" alt="skull" style="float:left;">
-<img src="images/skullspining.png" alt="skull" style="float:right;">
-<h1>Juegos Legales</h1>
-</div>
-
-<div class="topmenu">
-    <a href="index.php">Home</a>
-    <a href="juegos.php">Juegos</a>
-    <a href="login.html">Log in</a>
-    <a href="signin.html">Sign in</a>
-    <a href="modificardatos.php">Datos personales</a>
-    <a href="about.html">About</a>
-</div>
-
-
 <?php
 $host = "db";
 $usuario = "juegosacceso";
@@ -44,22 +18,45 @@ $user=$_POST['nombre'];
 $pw=$_POST['pw'];
 
 $q = "Select * from usuarios WHERE Nombre='".$user."' AND PW='".$pw."'" ;
-$qDNI = "Select DNI from usuarios WHERE Nombre='".$user."' AND PW='".$pw."'" ;
+$qDNI = "Select DNI,Nombre from usuarios WHERE Nombre='".$user."' AND PW='".$pw."'" ;
 $dame = mysqli_fetch_assoc(mysqli_query($dbconnect, $qDNI));
-$resultado=mysqli_query($dbconnect, $q);
+$resultado=mysqli_query($dbconnect, $q); }
 if(mysqli_num_rows($resultado)==1){
 setcookie("IdentComo",$dame['DNI'],time()+10000);
-echo "<h2>Usuario identificado</h2>
-    	<div class='cajaborrado'>
+setcookie("Usuario",$dame['Nombre'],time()+10000);
+echo '	<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="scripts/comprobar2.js"></script>
+<link rel="stylesheet" href="styles.css">
+<link rel="icon" type="image/x-icon" href="images/favicon.ico">
+<p><strong> Hola '.$_COOKIE["Usuario"].' </strong></p>
+</head>
+<body>
+<div class="toptitle">
+<img src="images/skullspining.png" alt="skull" style="float:left;">
+<img src="images/skullspining.png" alt="skull" style="float:right;">
+<h1>Juegos Legales</h1>
+</div>
+<div class="topmenu">
+    <a href="index.php">Home</a>
+    <a href="juegos.php">Juegos</a>
+    <a href="login.html">Log in</a>
+    <a href="signin.html">Sign in</a>
+    <a href="modificardatos.php">Datos personales</a>
+    <a href="about.html">About</a>
+</div>
+</body>
+</html>
+	<h2>Usuario identificado</h2>
+    	<div class="cajaborrado">
     		<br>
-    		<p>Te has identificado correctamente.</p>
+    		<p>Te has identificado correctamente como '.$user.'</p>
     		<br>
-    		<a href=juegos.php class='enlacecentral'>Volver a Juegos</a>
-    	</div>";
-}
+    		<a href=juegos.php class="enlacecentral">Volver a Juegos</a>
+    	</div>';
 }
 mysqli_close($dbconnect);
 ?>
-
 </body>
 </html>
