7b6bf49cfeed8ed57940f1b73c0966b6ec2bd36b
cursedx0 <104449990+cursedx0@users.noreply.github.com>
2023-11-25 23:12:45 +0100

    Parametrización de queries
    Parametrización de queries y restricción extra añadida por el lado del servidor a la hora de registrarse.

--- registrado.php
+++ registrado.php
@@ -33,7 +33,8 @@ $host = "db";
 $usuario = "juegosacceso";
 $contrasena = "admin";
 $maria = "juegos";
-$dbconnect=mysqli_connect($host,$usuario,$contrasena,$maria);
+//$dbconnect=mysqli_connect($host,$usuario,$contrasena,$maria);
+$dbconnect = new mysqli($host,$usuario,$contrasena,$maria);
 if($dbconnect->connect_error){
 	die("error de conexion");
 	echo "<h2>Usuario no registrado</h2>
@@ -54,8 +55,13 @@ $dni=$_POST['dni'];
 $tele=$_POST['telefono'];
 $fechan=$_POST['fechanac'];
 $email=$_POST['email'];
-$q = "INSERT INTO usuarios (Nombre, Apellido, PW, DNI, Telefono, Fechanac, email) VALUES ('$nombr', '$ape', '$pw', '$dni','$tele','$fechan','$email')";
-mysqli_query($dbconnect, $q);
+//$q = "INSERT INTO usuarios (Nombre, Apellido, PW, DNI, Telefono, Fechanac, email) VALUES ('$nombr', '$ape', '$pw', '$dni','$tele','$fechan','$email')";
+//mysqli_query($dbconnect, $q);
+if(!preg_match("/^\d{8}/",$dni)or(!preg_match("/[A-Z]{1}$/",$dni))or(!preg_match("/\d{9}$/",$tele))or(!preg_match("/\d{4}-\d{2}-\d{2}/",$fechanac))or(!preg_match("/[a-z]$/",$email))){
+
+$stmt = $dbconnect->prepare("INSERT INTO usuarios (Nombre, Apellido, PW, DNI, Telefono, Fechanac, email) VALUES (?,?,?,?,?,?,?)");
+$stmt->bind_param("sssssss",$nombr,$ape,$pw,$dni,$tele,$fechan,$email);
+$stmt->execute();
 echo "<h2>Usuario registrado</h2>
     	<div class='cajaborrado'>
     		<br>
@@ -63,8 +69,18 @@ echo "<h2>Usuario registrado</h2>
     		<br>
     		<a href=juegos.php class='enlacecentral'>Volver a Juegos</a>
     	</div>";
+}else{
+	echo "<h2>Usuario no registrado</h2>
+    	<div class='cajaborrado'>
+    		<br>
+    		<p>Ha habido un problema con el registro.</p>
+    		<br>
+    		<a href=juegos.php class='enlacecentral'>Volver a Juegos</a>
+    	</div>";	
+    }
 }
 mysqli_close($dbconnect);
+$stmt->close();
 ?>
 
 </body>
